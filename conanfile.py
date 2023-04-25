40a26e7c380c12c0642a824378d6dd3ca14cabe1
Ulrich Eck <ulrich.eck@tum.de>
2023-04-25 23:47:10 +0200

    new conan 2 package: teaserpp

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,23 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.scm import Git
+from conan.tools.files import load, update_conandata, copy, replace_in_file, collect_libs, get
 import os
-from io import StringIO
-import shutil
-from glob import glob
 
 
-class Open3dConan(ConanFile):
+class TeaserPPConan(ConanFile):
     version = "2.0"
 
     name = "teaserplusplus"
     license = "https://github.com/IntelVCL/Open3D/raw/master/LICENSE"
     description = "TEASER++ is a fast and certifiably-robust point cloud registration library written in C++, with Python and MATLAB bindings."
     url = "https://github.com/MIT-SPARK/TEASER-plusplus"
+
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
 
     requires = (
-        "eigen/[>=3.3.9]@camposs/stable",
-        "Boost/1.75.0@camposs/stable",
-        # "pcl/1.11.1-r1@camposs/stable",
-        
+        "eigen/3.4.0",
+        "boost/1.81.0",
         )
 
     options = {
@@ -27,61 +25,72 @@ class Open3dConan(ConanFile):
         "fPIC": [True, False],
         }
 
-    default_options = (
-        "shared=True",
-        "fPIC=True",
-        )
+    default_options = {
+        "shared": True,
+        "fPIC": True,
+    }
 
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
-    
     def source(self):
-        source_url = "https://github.com/MIT-SPARK/TEASER-plusplus/archive/refs/tags/v{}.tar.gz".format(self.version)
-        archive_name = "TEASER-plusplus-{0}".format(self.version)
-        tools.get(source_url)
-        os.rename(archive_name, self.source_subfolder)
+        get(self,
+            "https://github.com/MIT-SPARK/TEASER-plusplus/archive/refs/tags/v{}.tar.gz".format(self.version),
+            strip_root=True)
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+
+        def add_cmake_option(option, value):
+            var_name = "{}".format(option).upper()
+            value_str = "{}".format(value)
+            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str
+            tc.variables[var_name] = var_value
+
+        for option, value in self.options.items():
+            add_cmake_option(option, value)
+
+        tc.cache_variables["BUILD_TESTS"] = False
+        tc.cache_variables["BUILD_WITH_MARCH_NATIVE"] = False
+        tc.cache_variables["BUILD_PYTHON_BINDINGS"] = False
+        tc.cache_variables["BUILD_TEASER_FPFH"] = False
+
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def layout(self):
+        cmake_layout(self, src_folder="source_folder")
 
     def build(self):
-        teaserpp_source_dir = os.path.join(self.source_folder, self.source_subfolder)
-
-        tools.replace_in_file(os.path.join(self.source_subfolder, "CMakeLists.txt"),
-            """find_package(Eigen3 3.2 QUIET REQUIRED NO_MODULE)""",
-            """include(${CMAKE_BINARY_DIR}/../conanbuildinfo.cmake)
-conan_basic_setup()
-
-SET(EIGEN3_INCLUDE_DIRS "${CONAN_INCLUDE_DIRS_EIGEN}")
-MESSAGE(STATUS "Eigen: ${EIGEN3_FOUND} inc: ${EIGEN3_INCLUDE_DIRS}")
-SET(PCL_INCLUDE_DIRS "${CONAN_INCLUDE_DIRS_PCL}")
-SET(PCL_LIBRARY_DIRS "${CONAN_LIB_DIRS_PCL}")
-SET(PCL_LIBRARIES "${CONAN_LIBS_PCL}")
-MESSAGE(STATUS "PCL: ${CONAN_LIB_DIRS_PCL} inc: ${CONAN_INCLUDE_DIRS_PCL} lib: ${PCL_LIBRARIES}")
-find_package(Eigen3 QUIET REQUIRED NO_MODULE)""")
-
-        tools.replace_in_file(os.path.join(self.source_subfolder, "CMakeLists.txt"),
-            """find_package(PCL 1.8 QUIET REQUIRED COMPONENTS common io features kdtree)""",
-            """find_package(PCL QUIET REQUIRED COMPONENTS common io features kdtree)""")
-
         cmake = CMake(self)
-
-        cmake.definitions["BUILD_TESTS"] = False
-        cmake.definitions["BUILD_WITH_MARCH_NATIVE"] = False
-        cmake.definitions["BUILD_PYTHON_BINDINGS"] = False
-        cmake.definitions["BUILD_TEASER_FPFH"] = False
-
-        cmake.configure(source_folder="source_subfolder", build_folder="build_subfolder")
+        cmake.configure()
         cmake.build()
-        cmake.install()
 
     def package(self):
-
-        #self.copy(pattern="*", src="bin", dst="./bin")
-        self.copy(pattern="*.a", dst="lib", src=self.build_subfolder, keep_path=False)
-        self.copy(pattern="*.so", dst="lib", src=self.build_subfolder, keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", src=self.build_subfolder, keep_path=False)
-
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
-        libs = tools.collect_libs(self)
-        self.cpp_info.libs = libs
-        # self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
+        self.cpp_info.libs = collect_libs(self)
+
+#
+#     def build(self):
+#         teaserpp_source_dir = os.path.join(self.source_folder, self.source_subfolder)
+#
+#         tools.replace_in_file(os.path.join(self.source_subfolder, "CMakeLists.txt"),
+#             """find_package(Eigen3 3.2 QUIET REQUIRED NO_MODULE)""",
+#             """include(${CMAKE_BINARY_DIR}/../conanbuildinfo.cmake)
+# conan_basic_setup()
+#
+# SET(EIGEN3_INCLUDE_DIRS "${CONAN_INCLUDE_DIRS_EIGEN}")
+# MESSAGE(STATUS "Eigen: ${EIGEN3_FOUND} inc: ${EIGEN3_INCLUDE_DIRS}")
+# SET(PCL_INCLUDE_DIRS "${CONAN_INCLUDE_DIRS_PCL}")
+# SET(PCL_LIBRARY_DIRS "${CONAN_LIB_DIRS_PCL}")
+# SET(PCL_LIBRARIES "${CONAN_LIBS_PCL}")
+# MESSAGE(STATUS "PCL: ${CONAN_LIB_DIRS_PCL} inc: ${CONAN_INCLUDE_DIRS_PCL} lib: ${PCL_LIBRARIES}")
+# find_package(Eigen3 QUIET REQUIRED NO_MODULE)""")
+#
+#         tools.replace_in_file(os.path.join(self.source_subfolder, "CMakeLists.txt"),
+#             """find_package(PCL 1.8 QUIET REQUIRED COMPONENTS common io features kdtree)""",
+#             """find_package(PCL QUIET REQUIRED COMPONENTS common io features kdtree)""")
+#
+
